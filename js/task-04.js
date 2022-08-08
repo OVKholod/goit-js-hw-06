a43abf3a5c9257ae9c8142ce9cc5c103a7e62910
Oleksandr Kholod <avxolod@gmail.com>
2022-08-09 00:01:45 +0300

    done task-04

--- js/task-04.js
+++ js/task-04.js
@@ -2,4 +2,12 @@ let decrementBtn = document.querySelector('button[data-action="decrement"]');
 let incrementBtn = document.querySelector('button[data-action="increment"]');
 let counterValue = document.querySelector("#value");
 
-console.log(decrementBtn);
+let count = 0;
+
+decrementBtn.addEventListener("click", () => {
+  counterValue.textContent = --count;
+});
+
+incrementBtn.addEventListener("click", () => {
+  counterValue.textContent = ++count;
+});
