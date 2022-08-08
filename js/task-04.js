3c5f246032ed1fcf9f3644dfbf1df535fabf9ff7
Oleksandr Kholod <avxolod@gmail.com>
2022-08-08 23:39:23 +0300

    start task-04

---
+++ js/task-04.js
@@ -0,0 +1,5 @@
+let decrementBtn = document.querySelector('button[data-action="decrement"]');
+let incrementBtn = document.querySelector('button[data-action="increment"]');
+let counterValue = document.querySelector("#value");
+
+console.log(decrementBtn);
