f973989396d448997e48d21f7b483fdb4e9786b7
Oleksandr Kholod <avxolod@gmail.com>
2022-08-14 22:31:16 +0300

    done task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const formElements = e.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    email,
+    password,
+  };
+
+  if (email.length == 0 || password.length == 0) {
+    return alert`Заповніть всі поля`;
+  } else {
+    console.log(formData);
+    e.currentTarget.reset();
+  }
+}
