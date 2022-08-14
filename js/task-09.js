2fea8d8f902f033c96288224afe9b7b1fd1692dd
Oleksandr Kholod <avxolod@gmail.com>
2022-08-14 22:52:40 +0300

    done task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.body;
+const btn = document.querySelector(".change-color");
+const output = document.querySelector(".color");
+
+btn.addEventListener("click", onBtnClick);
+
+function onBtnClick(e) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  output.textContent = bodyEl.style.backgroundColor;
+}
